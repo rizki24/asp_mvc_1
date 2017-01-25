d83afd789e6eb013320753149e17cb18da521905
Rizki <rizkiamrullah38@gmail.com>
2017-01-25 08:03:44 +0700

    Add project files.

---
+++ asp_mvc_1/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using asp_mvc_1.Models.ViewModel;
+using asp_mvc_1.Models.EntityManager;
+
+namespace asp_mvc_1.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserSignUpView USV)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                if (!UM.IsLoginNameExist(USV.LoginName))
+                {
+                    UM.AddUserAccount(USV);
+                    FormsAuthentication.SetAuthCookie(USV.FirstName, false);
+                    return RedirectToAction("Welcome", "Home");
+
+                }
+                else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+    }
+}
---
+++ asp_mvc_1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace ssp_mvc_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
